012b9e78e127553bd233578e9da2ea4f70b6bdae
Ryan Mast <mast9@llnl.gov>
2023-02-16 10:36:22 -0800

    Collect file permission info

--- surfactant/fileinfo.py
+++ surfactant/fileinfo.py
@@ -1,4 +1,5 @@
 import os
+import stat
 from hashlib import sha256, sha1, md5
 
 
@@ -8,7 +9,19 @@ def get_file_info(filename):
     except FileNotFoundError:
         return None
     else:
-        return {"size": fstats.st_size, "accesstime": fstats.st_atime, "modifytime": fstats.st_mtime, "createtime": fstats.st_ctime}
+        filehidden = False
+        # stat.UF_HIDDEN (file shouldn't be shown in GUI macOS 10.5+)
+        if hasattr(fstats, "st_flags");
+            filehidden = bool(fstats.st_flags & stat.UF_HIDDEN)
+        # stat.FILE_ATTRIBUTE_HIDDEN (shouldn't be shown on Windows)
+        if hasattr(fstats, "st_file_attributes"):
+            filehidden = bool(fstats.st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
+        # Should consider if symlinks on Linux/macOS/Windows, or reparse points on Windows need to be recognized and handled
+        # lstat() does not follow symbolic links
+        # fstats.st_reparse_tag from os.lstat()
+        # stat.IO_REPARSE_TAG_SYMLINK
+        # stat.IO_REPARSE_TAG_MOUNT_POINT
+        return {"size": fstats.st_size, "accesstime": fstats.st_atime, "modifytime": fstats.st_mtime, "createtime": fstats.st_ctime, "filemode": stat.filemode(fstats.st_mode), "filehidden": filehidden}
     
 
 def calc_file_hashes(filename):
