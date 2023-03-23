c478584b0c79364403028cdd3b5741588395e4e7
Ryan Mast <mast9@llnl.gov>
2023-03-23 10:19:01 -0700

    Change indentation for JSON output to 2 spaces instead of 4

--- surfactant/output/cytrics_writer.py
+++ surfactant/output/cytrics_writer.py
@@ -5,4 +5,4 @@ from surfactant.sbomtypes import SBOM
 @surfactant.plugin.hookimpl
 def write_sbom(sbom: SBOM, outfile) -> None:
     # outfile is a file pointer, not a file name
-    outfile.write(sbom.to_json(indent=4))
+    outfile.write(sbom.to_json(indent=2))
