e5c2fc8e0b9befb38e12e7a4df72c0f34a18a553
Ross <ross63@llnl.gov>
2023-07-13 20:29:47 -0400

    Plugin working to push tar info to file metadata section

--- surfactantplugin-syft/surfactantplugin_syft.py
+++ surfactantplugin-syft/surfactantplugin_syft.py
@@ -1,19 +1,28 @@
 import surfactant.plugin, os, anchore_syft, subprocess, json
 from surfactant.sbomtypes import SBOM, Software
+from surfactant.plugin.manager import get_plugin_manager
 
 @surfactant.plugin.hookimpl
 def extract_file_info(sbom: SBOM, software: Software, filename: str, filetype: str) -> object:
-  print("TEST######################################")
-  if filetype in [".tar", ".sif"]:
-      data = subprocess.check_output('anchore_syft' + filename + '-o json', shell=True)
-      data = json.load(data)
-      info = {}
+  pm = get_plugin_manager()
+  #if filetype in [".tar", ".sif"]:
+  if filetype == "TAR":
+      data = subprocess.check_output('anchore_syft ' + filename + ' -o json', shell=True)
+      data = json.loads(data.decode())
+      #print(data)
+      #print("TEST######################################")
+      info = []
+      count = 1
       for i in data['artifacts']:
-         info['name'] = i['name']
-         info['version'] = i['version']
-         info['size'] = i['metaata']['installedSize']
-         info['vendor'] = i['maintainer']
-         info['metadata']['collectedBy'] = 'Syft: ' + i['foundBy']
+        testblock = {}
+        count = count + 1
+        testblock['name'] = i['name']
+        testblock['version'] = i['version']
+        testblock['size'] = i['metadata']['installedSize']
+        testblock['vendor'] = i['metadata']['maintainer']
+        #info['metadata']['collectedBy'] = 'Syft: ' + i['foundBy']
+        info.append(testblock)
+        print(info)
       return info
   else:
     return None
