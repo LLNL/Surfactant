245c9e742a27f826bf4c2ff5831a2ce44d793f5b
Doug Dennis <doug.dennis@pnnl.gov>
2023-09-06 14:03:22 -0700

    create test file for generate

--- tests/cmd/test_generate.py
+++ tests/cmd/test_generate.py
@@ -29,10 +29,23 @@ def test_generate_no_install_prefix(tmp_path):
     actual_software_names = {software["fileName"][0] for software in generated_sbom["software"]}
     assert expected_software_names == actual_software_names
 
+    expected_install_paths = {
+        "hello_world.exe": extract_path + "/hello_world.exe",
+        "testlib.dll": extract_path + "/testlib.dll",
+    }
     for software in generated_sbom["software"]:
-        assert software["installPath"] == []
+        assert (
+            software["installPath"][0]
+            == expected_install_paths[software["fileName"][0]]
+        )
 
-    assert len(generated_sbom["relationships"]) == 0
+    uuids = {software["fileName"][0]: software["UUID"] for software in generated_sbom["software"]}
+    assert len(generated_sbom["relationships"]) == 1
+    assert generated_sbom["relationships"][0] == {
+        "xUUID": uuids["hello_world.exe"],
+        "yUUID": uuids["testlib.dll"],
+        "relationship": "Uses",
+    }
 
 
 def test_generate_with_install_prefix(tmp_path):
@@ -63,7 +76,10 @@ def test_generate_with_install_prefix(tmp_path):
         "testlib.dll": "test_prefix/testlib.dll",
     }
     for software in generated_sbom["software"]:
-        assert software["installPath"][0] == expected_install_paths[software["fileName"][0]]
+        assert (
+            software["installPath"][0]
+            == expected_install_paths[software["fileName"][0]]
+        )
 
     uuids = {software["fileName"][0]: software["UUID"] for software in generated_sbom["software"]}
     assert len(generated_sbom["relationships"]) == 1
@@ -72,3 +88,32 @@ def test_generate_with_install_prefix(tmp_path):
         "yUUID": uuids["testlib.dll"],
         "relationship": "Uses",
     }
+
+
+def test_generate_with_skip_install_path(tmp_path):
+    extract_path = Path(testing_data, "Windows_dll_test_no1").as_posix()
+    config_data = f'[{{"extractPaths": ["{extract_path}"]}}]'
+    config_path = str(Path(tmp_path, "config.json"))
+    output_path = str(Path(tmp_path, "out.json"))
+
+    with open(config_path, "w") as f:
+        f.write(config_data)
+
+    # the click.testing module would be better here but it doesn't allow for files to be generated
+    # pylint: disable=no-value-for-parameter
+    sbom(["--skip_install_path", config_path, output_path], standalone_mode=False)
+    # pylint: enable
+
+    with open(output_path) as f:
+        generated_sbom = json.load(f)
+
+    assert len(generated_sbom["software"]) == 2
+
+    expected_software_names = {"hello_world.exe", "testlib.dll"}
+    actual_software_names = {software["fileName"][0] for software in generated_sbom["software"]}
+    assert expected_software_names == actual_software_names
+
+    for software in generated_sbom["software"]:
+        assert software["installPath"] == []
+
+    assert len(generated_sbom["relationships"]) == 0
