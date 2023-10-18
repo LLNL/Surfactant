1e21f3d3c78984ec9f2361f71d7ae5f7a55c7c1f
Williams, Tyler J <tyler.williams@pnnl.gov>
2023-10-18 10:37:41 -0400

    updated to streamline config creation

--- surfactant/cmd/createconfig.py
+++ surfactant/cmd/createconfig.py
@@ -21,24 +21,10 @@ from loguru import logger
 )
 def create_config(directory, output, install_prefix):
     """Create surfactant input configuration file based on input directory."""
-    starting_directory = Path(directory)
-    extract_paths = []
-
-    # Check if there are any files in the directory
-    if any(item.is_file() for item in starting_directory.iterdir()):
-        extract_paths.append(starting_directory.as_posix())
-    else:
-        # If there are no files, add the immediate subdirectories
-        for item in starting_directory.iterdir():
-            if item.is_dir():
-                extract_paths.append(item.as_posix())
-
+    extract_paths = [Path(directory).as_posix()]
     config_dict = {"extractPaths": extract_paths, "installPrefix": install_prefix}
     config_out = [config_dict]
-
-    output_file_name = output or starting_directory.stem + ".json"
-
+    output_file_name = output or Path(directory).stem + ".json"
     with open(output_file_name, "w") as json_file:
         json.dump(config_out, json_file, indent=4)
-
     logger.info(f"Data written to {output_file_name}")
