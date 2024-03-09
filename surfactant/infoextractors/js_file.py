1d8a8cc9571cf3e9cd3ccb5ef90135b346c0c37c
Carlos Zatarain <Niarataz01@gmail.com>
2024-03-09 00:15:48 -0600

    Local test files work: Sbom outputs select2 as correct library detected from file contents

--- surfactant/infoextractors/js_file.py
+++ surfactant/infoextractors/js_file.py
@@ -1,5 +1,13 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC
+# SEe the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
 import json
+import pathlib
 import re
+from typing import Any, Dict
+
+from loguru import logger
 
 import surfactant.plugin
 from surfactant.sbomtypes import SBOM, Software
@@ -19,4 +27,38 @@ def extract_file_info(
 
 
 def extract_js_info(filename):
-    return {"JavascriptTest": "Hello World!"}
+    js_info: Dict[str, Any] = {"Library": {}}
+    js_lib_file = pathlib.Path(__file__).parent / "js_libraries.regex"
+
+    # Load expressions from retire.js, should move this file elsewhere
+    try:
+        with open(js_lib_file, "r") as regex:
+            database = json.load(regex)
+    except FileNotFoundError:
+        logger.warning(f"File not found: {js_lib_file}")
+        return None
+
+    # Try to match file name
+    libname = match_by_attribute("filename", filename, database)
+    if libname is not None:
+        js_info["Library"]["By_Filename"] = libname
+
+    # Try to match file contents
+    try:
+        with open(filename, "r") as js_file:
+            filecontent = js_file.read()
+        libname = match_by_attribute("filecontent", filecontent, database)
+        if libname is not None:
+            js_info["Library"]["By_File_Contents"] = libname
+    except FileNotFoundError:
+        logger.warning(f"File not found: {filename}")
+    return js_info
+
+
+def match_by_attribute(attribute: str, content: str, database: Dict) -> str:
+    for name, library in database.items():
+        if attribute in library:
+            for pattern in library[attribute]:
+                if re.search(pattern, content):
+                    return name
+    return None
