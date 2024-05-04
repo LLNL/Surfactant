42f6ea70a8e49c76ea3f9b16cd686457f6b05779
Carlos Zatarain <Niarataz01@gmail.com>
2024-05-04 17:05:03 -0500

    Added signature and library dependency information

--- surfactant/infoextractors/mach_o_file.py
+++ surfactant/infoextractors/mach_o_file.py
@@ -36,29 +36,63 @@ def extract_mach_o_info(filename):
 
     # Iterate over all binaries in the FAT binary
     for binary in binaries:
-        header = binary.header
         binary_details = {}
-
         binary_details["format"] = binary.format.__name__
 
         # Extract info from headers
-        binary_details["cpuType"] = header.cpu_type.__name__
-        binary_details["cpuSubtype"] = header.cpu_subtype
-        binary_details["fileType"] = header.file_type.__name__
-        binary_details["flags"] = [flag.__name__ for flag in header.flags_list]
-        binary_details["numCommands"] = header.nb_cmds
+        header = binary.header
+        header_details = {}
+        header_details["cpuType"] = header.cpu_type.__name__
+        header_details["cpuSubtype"] = header.cpu_subtype
+        header_details["fileType"] = header.file_type.__name__
+        header_details["flags"] = [flag.__name__ for flag in header.flags_list]
+        header_details["numCommands"] = header.nb_cmds
+        binary_details["header"] = header_details
 
         # Extract info from Build Version
-        if binary.has_build_version():
+        if binary.has_build_version:
             build = binary.build_version
-            binary_details["platform"] = build.platform.__name__
-            binary_details["minOSVersion"] = build.minos
-            binary_details["sdkVersion"] = build.sdk
+            build_details = {}
+            build_details["platform"] = build.platform.__name__
+            build_details["minOSVersion"] = build.minos
+            build_details["sdkVersion"] = build.sdk
 
             tools = []
             for tool in build.tools:
                 tools.append({"tool": tool.tool, "version": tool.version})
-            binary_details["tools"] = tools
+            build_details["tools"] = tools
+            binary_details["build"] = build_details
+
+        # Extract info from Code Signature
+        if binary.has_code_signature:
+            signature = binary.code_signature
+            signature_details = {}
+            signature_details["signatureSize"] = signature.data_size
+            signature_details["signatureOffset"] = signature.data_offset
+            # signature_details["signature"] = signature.content
+            binary_details["codeSignature"] = signature_details
+
+        # This is the case if it was signed with LC_DYLIB_CODE_SIGN_DRS
+        if binary.has_code_signature_dir:
+            signature = binary.code_signature_dir
+            signature_details = {}
+            signature_details["signatureSize"] = signature.data_size
+            signature_details["signatureOffset"] = signature.data_offset
+            # signature_details["signature"] = signature.content
+            binary_details["codeSignatureDir"] = signature_details
+
+        # Extract library dependencies
+        libraries = []
+        for library in binary.libraries:
+            lib_details = {}
+            lib_details["name"] = library.name
+            lib_details["currentVersion"] = library.current_version
+            lib_details["compatibilityVersion"] = library.compatibility_version
+            libraries.append(lib_details)
+        binary_details["libraryDependencies"] = libraries
+            
+        
+            
 
         file_details["binaries"].append(binary_details)
 
