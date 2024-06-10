84026028bb1818c5a4a68444801f0df0f76a076c
Kendall Harter <harter8@llnl.gov>
2024-06-10 02:18:01 -0700

    Catch FileNotFound exception when trying to run Docker Scout

--- surfactant/infoextractors/docker_image.py
+++ surfactant/infoextractors/docker_image.py
@@ -15,11 +15,14 @@ from surfactant.sbomtypes import SBOM, Software
 
 def is_docker_scout_installed():
     # Check that Docker Scout can be run
-    result = subprocess.run(["docker", "scout", "--help"], capture_output=True, check=False)
-    if result.returncode != 0:
-        logger.warning("Install Docker Scout to scan containers for additional information")
+    try:
+        result = subprocess.run(["docker", "scout", "--help"], capture_output=True, check=False)
+        if result.returncode != 0:
+            logger.warning("Install Docker Scout to scan containers for additional information")
+            return False
+        return True
+    except FileNotFoundError:
         return False
-    return True
 
 
 # Check if Docker Scout is installed when this Python module gets loaded
