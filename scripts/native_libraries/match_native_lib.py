416d37eb2aad5154f4013f1887efa078385eea5a
Wangmo Tenzing <tenzing1@llnl.gov>
2024-08-12 00:12:49 -0700

    testing with more native libs

--- scripts/native_libraries/match_native_lib.py
+++ scripts/native_libraries/match_native_lib.py
@@ -2,14 +2,14 @@ import json
 import re
 
 
-def find_js_match(expressions: dict, filename: str) -> str:
+def find_native_match(expressions: dict, filename: str) -> str:
     for name, library in expressions.items():
         if "filename" in library:
             for pattern in library["filename"]:
                 if re.search(pattern, filename):
                     return name
     try:
-        with open(filename, "r") as jsfile:
+        with open(filename, "r", encoding="ISO-8859-1") as jsfile:
             contents = jsfile.read()
         for name, library in expressions.items():
             if "filecontent" in library:
@@ -25,7 +25,8 @@ def find_js_match(expressions: dict, filename: str) -> str:
 with open("/Users/tenzing1/surfactant_new_venv/Surfactant/surfactant/infoextractors/native_lib_patterns.json", "r") as f:
     patterns = json.load(f)
 
+
 #print("this is patterns: ", patterns)
 
-library_name = find_js_match(patterns, "7z_win32.exe")
+library_name = find_native_match(patterns, "binutils-2.13.92.tar.bz2")
 print(library_name)
