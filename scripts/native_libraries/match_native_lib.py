261bec9cf953065379e5c10065779b87ac1b4214
Wangmo Tenzing <tenzing1@llnl.gov>
2024-08-14 14:05:37 -0700

    making some updates to matching script

--- scripts/native_libraries/match_native_lib.py
+++ scripts/native_libraries/match_native_lib.py
@@ -6,28 +6,33 @@ def find_native_match(expressions: dict, filename: str) -> str:
     for name, library in expressions.items():
         if "filename" in library:
             for pattern in library["filename"]:
-                if re.search(pattern, filename):
-                    print("found through filename")
-                    return name
+                try:
+                    if re.search(pattern, filename):
+                        print("found through filename")
+                        return name
+                except:
+                    breakpoint()
+
     try:
         with open(filename, "r", encoding="ISO-8859-1") as nativefile:
             contents = nativefile.read()
+        print("this is contents: ", contents)
         for name, library in expressions.items():
             if "filecontent" in library:
                 for pattern in library["filecontent"]:
                     try:
                         if re.search(pattern, contents):
                             print("found through filecontent")
+                            print("pattern")
                             return name
-                    except:
-                        breakpoint()
+                    except re.error as e:
+                        print(f"Regex error with pattern '{pattern}': {e}")                        
     except FileNotFoundError:
         print(f"File not found: {filename}")
     return None
 
-
 with open("/Users/tenzing1/surfactant_new_venv/Surfactant/surfactant/infoextractors/native_lib_patterns.json", "r") as f:
     patterns = json.load(f)
 
-library_name = find_native_match(patterns, "binutils-2.13.92.tar.bz2")
+library_name = find_native_match(patterns, "/Users/tenzing1/surfactant_new_venv/Surfactant/scripts/native_libraries/busybox-1_36_1.tar.bz2")
 print(library_name)
