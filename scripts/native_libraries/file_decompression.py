6760fe25dee42ba3de30c27105e8b408b5429b71
Wangmo Tenzing <tenzing1@llnl.gov>
2024-08-26 01:14:24 -0700

    adding different compression types

--- scripts/native_libraries/file_decompression.py
+++ scripts/native_libraries/file_decompression.py
@@ -22,19 +22,20 @@ def check_compression_type(filename):
     elif filename.endswith('.tar.gz'):
         mode = 'r:gz'
     elif filename.endswith('.tar.bz2'):
+        print("hello")
         mode = 'r:bz2'
     elif filename.endswith('.tar.xz'):
         mode = 'r:xz'
     else:
         print('not supported')
-        return
     
     if mode:
+        print("we have mode")
         decompress_tar_file(filename, mode)
 
 def create_temp_dir():
     # Create a temporary directory
-    temp_dir = tempfile.mkdtemp()
+    temp_dir = tempfile.mkdtemp(prefix='surfactant-temp')
     #with tempfile.TemporaryDirectory() as temp:
     return temp_dir
 
@@ -48,50 +49,26 @@ def decompress_tar_file(filename, compression_type):
     temp_dir = create_temp_dir()
     with tarfile.open(filename, compression_type) as tar:
         # insert extract path
-        tar.exrtactall(path=temp_dir)
+        tar.extractall(path=temp_dir)
+        print("extracted")
         # return extracted file to native_lib_file ? so the func should return this dir
 
 def open_tar_file(filename):
     #use temp dir
     temp_dir = create_temp_dir()
     try:
-        if tar_file():
-            contents = {}
-            with tarfile.open(filename, 'r') as tar:
-                print("Opened tarfile")
-                tar.extractall(path=temp_dir)
-                print("All files extracted")
+        contents = {}
+        with tarfile.open(filename, 'r') as tar:
+            print("Opened tarfile")
+            tar.extractall(path=temp_dir)
+            print("All files extracted")
                                
     except FileNotFoundError:
         print(f"File not found: {filename}")
 
 
-        # if filename.endswith('.tar'):
-        #     with tarfile.open(filename, 'r') as tar:
-                
-        #         print("Opened tarfile")
-        #     tar.extractall(path="./extraction_dir")
-        #     print("All files extracted")
-
-        # else:    
-        #     with tarfile.open(filename, mode) as tar:
-        #         tar.extractall(path="/Users/tenzing1/surfactant_new_venv/Surfactant/scripts/native_libraries/decompressed_files_3")
 
 
 
-tar_file = open_tar_file("/Users/tenzing1/surfactant_new_venv/Surfactant/scripts/native_libraries/bgpd.pl-0.08.tar")
-print(tar_file)
-
-
-#try to use tempfile module here so it takes care of cleanup
-        # with tarfile.open(filename, "r:bz2") as tar:
-        #     tar.extractall(path="/Users/tenzing1/surfactant_new_venv/Surfactant/scripts/native_libraries/new_files")
-        
-        #     # Save all decompressed files in temp dir
-        #     for root, dirs, files in os.walk("/Users/tenzing1/surfactant_new_venv/Surfactant/scripts/native_libraries/new_files"):
-        #         for file_name in files:
-        #             file_path = os.path.join(root, file_name)             
-    
-    # Delete all temp files after matches are found to keep dir clean?  
-    # 
-    # 
+# tar_file = open_tar_file("/Users/tenzing1/surfactant_new_venv/Surfactant/scripts/native_libraries/bgpd.pl-0.08.tar")
+# print(tar_file)
