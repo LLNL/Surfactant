e048e20ece607d667851cc886cc1a44c823a1211
Shayna Kapadia <kapadia2@llnl.gov>
2024-09-23 15:44:52 -0700

    Forgot to add init.py

---
+++ surfactant/cmd/cli_commands/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from .cli_add import Load
+from .cli_find import Save
+
+__all__ = [
+    "Load",
+    "Save"
+]
