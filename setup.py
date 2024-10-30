5ba4615f2472d4a38382b6988b4c9345a6cc1276
Moses-Mk <mosesmakuei2520@gmail.com>
2024-10-30 16:51:47 +0530

    updated setup()

--- setup.py
+++ setup.py
@@ -3,22 +3,7 @@
 #
 # SPDX-License-Identifier: MIT
 
+# For compatibility with old versions of tools
 from setuptools import setup
 
-setup(
-    #Other fields
-    
-    extras_require={
-        "docs": [
-            "Sphinx>=4.0",             # Documentation generator
-            "sphinx-rtd-theme",        # Read the Docs theme
-            "surfactant @ file:.\Surfactant",  # Specify path if needed
-            "sphinx.ext.autodoc",      # For automatic API documentation
-            "sphinx.ext.napoleon",     # For Google style docstrings
-            "sphinx.ext.viewcode",     # To include source code in documentation
-            "sphinx.ext.intersphinx",   # For linking to other projects' documentation
-            "sphinx.ext.githubpages"    # For GitHub Pages integration
-        ]
-    },
-    # Other setup fields as needed (author, description, etc.)
-)
+setup()
