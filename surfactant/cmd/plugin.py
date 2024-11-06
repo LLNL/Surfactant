4d27807127f6e15ca2d61f914b19baf73fa3228f
Willis Berrios <berrios1@llnl.gov>
2024-11-06 20:51:44 +0000

    Added print disabled plugins to list subcommand.

--- surfactant/cmd/plugin.py
+++ surfactant/cmd/plugin.py
@@ -21,7 +21,7 @@ def plugin_list_cmd():
     current_blocked_plugins = config_manager.get(section, section_key, [])
     
     print("-------")
-    print("Disabled Plugins")
+    print("DISABLED PLUGINS")
     for disabled_plugin in current_blocked_plugins :
         print("-------")
         print(f"name: {disabled_plugin}")
