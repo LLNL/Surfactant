62657de240b59c550f29ea7c03bb63cd9798eca1
Willis Berrios <berrios1@llnl.gov>
2024-11-11 15:22:34 +0000

    Added plugin enable functionality.

--- surfactant/cmd/plugin.py
+++ surfactant/cmd/plugin.py
@@ -1,5 +1,8 @@
 import click
+from typing import List
+from surfactant.plugin.manager import get_plugin_manager
 from surfactant.configmanager import ConfigManager
+
 @click.command(name="list")
 def plugin_list_cmd():
     """Lists plugins."""
@@ -26,8 +29,34 @@ def plugin_list_cmd():
         print("-------")
         print(f"name: {disabled_plugin}")
 
-from typing import List
-from surfactant.plugin.manager import get_plugin_manager
+@click.command(name="enable")
+@click.argument('plugin_names', nargs=-1)
+def plugin_enable_cmd(plugin_names):
+    """Enables one or more plugins."""
+    if not plugin_names:
+        raise click.UsageError("At least one plugin name must be specified.")
+    section = 'core'
+    section_key = 'disable_plugins'
+
+    config_manager = ConfigManager()
+
+    # Retrieve the current list of plugins that should be blocked
+    current_blocked_plugins = config_manager.get(section, section_key, [])
+
+    # Ensure current_blocked_plugins is a list
+    if isinstance(current_blocked_plugins, str):
+        current_blocked_plugins = [current_blocked_plugins]
+
+    # Remove the plugin from the blocked list if present
+    for plugin_name in plugin_names:
+        if plugin_name in current_blocked_plugins:
+            current_blocked_plugins.remove(plugin_name)
+
+    # Update the configuration to remove plugins from being disabled
+    config_manager.set(section, section_key, current_blocked_plugins)
+    click.echo(f"Updated blocked plugins: {current_blocked_plugins}")
+
+
 @click.command(name="disable")
 @click.argument('plugin_names', nargs=-1)
 def plugin_disable_cmd(plugin_names):
@@ -45,9 +74,9 @@ def plugin_disable_cmd(plugin_names):
     # Ensure current_blocked_plugins is a list
     if isinstance(current_blocked_plugins, str):
         current_blocked_plugins = [current_blocked_plugins]
-	
+
     # Add the plugin to the blocked list if not already present
-    for plugin_name in plugin_names :
+    for plugin_name in plugin_names:
         if plugin_name not in current_blocked_plugins:
             current_blocked_plugins.append(plugin_name)
 
