07c41b02ac81eae0d363c4b0547827b1ff409564
Kendall Harter <harter8@llnl.gov>
2024-12-02 04:16:39 -0800

    TUI improvements

--- surfactant/cmd/config_tui.py
+++ surfactant/cmd/config_tui.py
@@ -88,14 +88,23 @@ class SelectFile(textual.screen.ModalScreen[Optional[textual.widgets.DirectoryTr
 
 
 class ExtractPathContent(textual.widgets.Static):
-    def __init__(self, path):
+    def __init__(self, parent):
         super().__init__()
-        self.path = path
+        self.par = parent
 
     def compose(self) -> textual.app.ComposeResult:
-        yield textual.widgets.Button("Delete Path", id="delete_path")
-        yield textual.widgets.Button("Select Path", id="select_path")
-        yield textual.widgets.Label(f"Path: {self.path}", id="path")
+        if len(self.par.path) > 0:
+            yield textual.widgets.Label(f"{self.par.path}", id="path")
+        else:
+            yield textual.widgets.Label(f"(Click to set extract path)", id="path")
+
+    def on_click(self):
+        def set_path(path: Optional[textual.widgets.DirectoryTree.FileSelected]):
+            if path:
+                self.par.path = path
+                self.get_child_by_id("path", textual.widgets.Label).update(f"Path: {self.par.path}")
+
+        self.app.push_screen(SelectFile(True), set_path)
 
 
 class ExtractPathSelector(textual.widgets.Static):
@@ -105,17 +114,8 @@ class ExtractPathSelector(textual.widgets.Static):
         self.alive = True
 
     def compose(self) -> textual.app.ComposeResult:
-        yield ExtractPathContent(self.path)
-        yield textual.widgets.Rule()
-
-    @textual.on(textual.widgets.Button.Pressed, "#select_path")
-    def on_select_path(self):
-        def set_path(path: Optional[textual.widgets.DirectoryTree.FileSelected]):
-            if path:
-                self.path = path
-                self.get_child_by_id("path", textual.widgets.Label).update(f"Path: {self.path}")
-
-        self.app.push_screen(SelectFile(True), set_path)
+        yield textual.widgets.Button("-", id="delete_path")
+        yield ExtractPathContent(self)
 
     @textual.on(textual.widgets.Button.Pressed, "#delete_path")
     def on_delete_path(self):
@@ -129,23 +129,24 @@ class ExtractPathAdder(textual.widgets.Static):
     def __init__(self, entry_id):
         super().__init__()
         self.paths = []
+        self.entry_id = entry_id
         for path in self.app.config_data[entry_id]["extractPaths"]:
             self.paths.append(ExtractPathSelector(path))
 
     def compose(self) -> textual.app.ComposeResult:
-        yield textual.widgets.Button("Add Extract Path", id="add_path")
+        yield textual.widgets.Button("+", id=f"add_path_{self.entry_id}")
 
     def on_mount(self):
         entries = self.app.query_one("#config_entries", textual.containers.ScrollableContainer)
         for path in self.paths:
             if path.alive:
-                entries.mount(path, before="#add_path")
+                entries.mount(path, before=f"#add_path_{self.entry_id}")
 
-    @textual.on(textual.widgets.Button.Pressed, "#add_path")
-    def on_add_path(self):
-        entries = self.app.query_one("#config_entries", textual.containers.ScrollableContainer)
-        self.paths.append(ExtractPathSelector(""))
-        entries.mount(self.paths[-1], before="#add_path")
+    def on_button_pressed(self, event: textual.widgets.Button.Pressed) -> None:
+        if event.button.id == f"#add_path_{self.entry_id}":
+            entries = self.app.query_one("#config_entries", textual.containers.ScrollableContainer)
+            self.paths.append(ExtractPathSelector(""))
+            entries.mount(self.paths[-1], before=f"#add_path_{self.entry_id}")
 
 
 class ArchiveEntry(textual.widgets.Static):
@@ -154,11 +155,12 @@ class ArchiveEntry(textual.widgets.Static):
         self.archive_loc = self.app.config_data[entry_id]["archive"]
 
     def compose(self) -> textual.app.ComposeResult:
-        yield textual.widgets.Button("Select Archive", id="archive_button")
-        yield textual.widgets.Label(f"Archive: {self.archive_loc}", id="archive_label")
+        if len(self.archive_loc) > 0:
+            yield textual.widgets.Label(f"Archive: {self.archive_loc}", id="archive_label")
+        else:
+            yield textual.widgets.Label("Archive: [Click to select archive]", id="archive_label")
 
-    @textual.on(textual.widgets.Button.Pressed, "#archive_button")
-    def on_archive_button(self):
+    def on_click(self):
         def set_archive(path: Optional[textual.widgets.DirectoryTree.FileSelected]):
             if path:
                 self.archive_loc = path
@@ -175,7 +177,7 @@ class InstallPrefix(textual.widgets.Static):
         self.install_prefix = self.app.config_data[entry_id]["installPrefix"]
 
     def compose(self) -> textual.app.ComposeResult:
-        yield textual.widgets.Label("Install Prefix:")
+        yield textual.widgets.Label("Install Prefix: ")
         yield textual.widgets.Input(self.install_prefix, id="prefix")
 
     @textual.on(textual.widgets.Input.Changed, "#prefix")
@@ -198,6 +200,7 @@ class ConfigEntry(textual.widgets.Static):
         yield textual.widgets.Button("Delete this entry", id="delete_entry")
         yield self.archive_entry
         yield self.install_prefix
+        yield textual.widgets.Label("Extract Paths:")
         yield self.extract_path_adder
 
     @textual.on(textual.widgets.Button.Pressed, "#delete_entry")
@@ -205,18 +208,29 @@ class ConfigEntry(textual.widgets.Static):
         def delete_self(do_it: Optional[bool]):
             if do_it:
                 self.remove()
-                entries = self.app.query_one(
-                    "#config_entries", textual.containers.ScrollableContainer
-                )
-                for child in entries.children:
-                    child.remove()
-                self.app.query_one(f"#view_entry_{self.entry_id}").remove()
+                # entries = self.app.query_one(
+                #     "#config_entries", textual.containers.ScrollableContainer
+                # )
+                # for child in entries.children:
+                #     child.remove()
+                # self.app.query_one(f"#view_entry_{self.entry_id}").remove()
                 self.alive = False
 
         self.app.push_screen(
             YesNoScreen("Are you sure you want to delete this entry?"), delete_self
         )
 
+
+def add_entry(app: textual.app.App) -> ConfigEntry:
+    if app.entry_id not in app.config_data:
+        app.config_data[app.entry_id] = {
+            "archive": "",
+            "extractPaths": [],
+            "installPrefix": "",
+        }
+    app.config_entries[app.entry_id] = ConfigEntry(app.entry_id)
+    app.entry_id += 1
+    return app.config_entries[app.entry_id - 1]
 
 class AddEntry(textual.widgets.Static):
     """A widget for adding another config entry"""
@@ -226,54 +240,7 @@ class AddEntry(textual.widgets.Static):
 
     @textual.on(textual.widgets.Button.Pressed, "#add_config_entry")
     def on_add_config_entry(self):
-        if self.app.entry_id not in self.app.config_data:
-            self.app.config_data[self.app.entry_id] = {
-                "archive": "",
-                "extractPaths": [],
-                "installPrefix": "",
-            }
-        add_entries = self.app.query_one("#add_entry_scrollable")
-        add_entries.mount(
-            textual.widgets.Button(str(self.app.entry_id), id=f"view_entry_{self.app.entry_id}")
-        )
-        self.app.config_entries[self.app.entry_id] = ConfigEntry(self.app.entry_id)
-        self.app.entry_id += 1
-
-
-class MainBody(textual.widgets.Static):
-    def __init__(self):
-        super().__init__()
-        self.selected_button_num = 0
-
-    def compose(self) -> textual.app.ComposeResult:
-        yield textual.containers.ScrollableContainer(id="config_entries")
-        yield textual.containers.ScrollableContainer(
-            AddEntry(id="add_entry"), id="add_entry_scrollable"
-        )
-
-    def on_button_pressed(self, event: textual.widgets.Button.Pressed) -> None:
-        START_BUTTON_TEXT = "view_entry_"
-        if button_id := event.button.id:
-            if button_id.startswith(START_BUTTON_TEXT):
-                button_num = int(button_id[len(START_BUTTON_TEXT) :])
-                # Remove old button selection
-                try:
-                    self.app.query_one(
-                        f"#{START_BUTTON_TEXT}{self.selected_button_num}", textual.widgets.Button
-                    ).remove_class("selected")
-                except textual.css.query.NoMatches:
-                    pass
-                event.button.add_class("selected")
-                entry = self.app.query_one(
-                    "#config_entries", textual.containers.ScrollableContainer
-                )
-
-                # Only change the panel if a new button was pressed
-                if self.selected_button_num != button_num:
-                    for child in entry.children:
-                        child.remove()
-                    entry.mount(self.app.config_entries[button_num])
-                self.selected_button_num = button_num
+        self.app.query_one("#config_entries").mount(add_entry(self.app), before="#add_config_entry")
 
 
 class ConfigTUI(textual.app.App):
@@ -304,7 +271,7 @@ class ConfigTUI(textual.app.App):
     def compose(self) -> textual.app.ComposeResult:
         yield textual.widgets.Header()
         yield textual.widgets.Footer()
-        yield MainBody()
+        yield textual.containers.ScrollableContainer(id="config_entries")
 
     def on_mount(self):
         for i, entry in enumerate(self.config_json):
@@ -316,7 +283,10 @@ class ConfigTUI(textual.app.App):
                 self.config_data[i]["extractPaths"] = entry["extractPaths"]
             if "installPrefix" in entry:
                 self.config_data[i]["installPrefix"] = entry["installPrefix"]
-            self.query_one("#add_entry", AddEntry).on_add_config_entry()
+            add_entry(self)
+        for entry in self.config_entries.values():
+            self.query_one("#config_entries").mount(entry)
+        self.query_one("#config_entries").mount(AddEntry())
 
     def action_toggle_dark(self) -> None:
         """A binding for toggling dark mode"""
@@ -326,21 +296,22 @@ class ConfigTUI(textual.app.App):
         """Saves the current SBOM to file"""
         to_save = []
         for entry in self.config_entries.values():
-            to_save.append({})
-            write_to = to_save[-1]
-            # Load data, ignoring it if empty
-            archive = entry.archive_entry.archive_loc
-            if len(archive) > 0:
-                write_to["archive"] = archive
-            install_prefix = entry.install_prefix.install_prefix
-            if len(install_prefix) > 0:
-                write_to["installPrefix"] = install_prefix
-            # Always write extract paths
-            write_to["extractPaths"] = []
-            extract_path_adder = entry.extract_path_adder
-            for selector in extract_path_adder.paths:
-                if selector.alive:
-                    write_to["extractPaths"].append(selector.path)
+            if entry.alive:
+                to_save.append({})
+                write_to = to_save[-1]
+                # Load data, ignoring it if empty
+                archive = entry.archive_entry.archive_loc
+                if len(archive) > 0:
+                    write_to["archive"] = archive
+                install_prefix = entry.install_prefix.install_prefix
+                if len(install_prefix) > 0:
+                    write_to["installPrefix"] = install_prefix
+                # Always write extract paths
+                write_to["extractPaths"] = []
+                extract_path_adder = entry.extract_path_adder
+                for selector in extract_path_adder.paths:
+                    if selector.alive:
+                        write_to["extractPaths"].append(selector.path)
 
         with open(self.config_path, "w") as f:
             f.write(json.dumps(to_save))
