85e4220c18b7723f715f589f2a8f04b6d532e9db
Ryan Mast <mast9@llnl.gov>
2025-01-23 11:25:33 -0800

    fix: crash when checking zlib magic bytes on empty or 1-byte files (#328)

--- surfactant/filetypeid/id_magic.py
+++ surfactant/filetypeid/id_magic.py
@@ -194,12 +194,13 @@ def identify_file_type(filepath: str) -> Optional[str]:
                 return "OMF_LIB"
             # zlib:
             # https://www.rfc-editor.org/rfc/rfc1950
-            cmf = magic_bytes[0]
-            flg = magic_bytes[1]
-            cm = cmf & 0x0F
-            if cm == 8:
-                if (cmf * 256 + flg) % 31 == 0:
-                    return "ZLIB"
+            if len(magic_bytes) >= 2:
+                cmf = magic_bytes[0]
+                flg = magic_bytes[1]
+                cm = cmf & 0x0F
+                if cm == 8:
+                    if (cmf * 256 + flg) % 31 == 0:
+                        return "ZLIB"
             return None
     except FileNotFoundError:
         return None
