24cf774ad4950546f4a14d9510f8bc616594165e
Willis Berrios <berrios1@llnl.gov>
2025-02-11 22:53:41 +0000

    Fixed typo.

--- surfactant/database_manager/database_utils.py
+++ surfactant/database_manager/database_utils.py
@@ -73,7 +73,7 @@ class BaseDatabaseManager(ABC):
                 self._database = json.load(db_file)
         except FileNotFoundError:
             logger.warning(
-                f"{self.config.database_key} database could not be loaded. Run `surfactant plugin update-db {self.plugin_name}` to fetch the database."
+                f"{self.config.database_key} database could not be loaded. Run `surfactant plugin update-db {self.config.plugin_name}` to fetch the database."
             )
             self._database = None
 
