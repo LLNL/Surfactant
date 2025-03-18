15336fb533c8af67ff5539ff584aa1a8b558daf2
Willis Berrios <berrios1@llnl.gov>
2025-03-18 17:10:39 +0000

    refactor: fixed pylint W1203: Use lazy % formatting in logging functions (logging-fstring-interpolation)

--- plugins/grype/test_grype.py
+++ plugins/grype/test_grype.py
@@ -32,7 +32,7 @@ def setup_environment():
     # Install the Grype plugin in editable mode
     logging.info("Installing the Grype plugin in editable mode ...")
     output = run_command("pip install -e .")
-    logging.info(f"Install output: {output}")
+    logging.info("Install output: '%s'", output)
 
     enable_plugin(PLUGIN_NAME)
 
@@ -46,7 +46,7 @@ def install_grype():
     """Install Grype if not already installed."""
     try:
         output = run_command("grype --version")
-        logging.info(f"Grype is already installed: {output}")
+        logging.info("Grype is already installed: '%s'", output)
     except RuntimeError:
         logging.info("Installing Grype...")
         run_command(
@@ -76,12 +76,12 @@ def create_config_and_tarball(tmp_path):
     The tarball contains the 'hello-world' Docker container filesystem.
     """
     # Pull the 'hello-world' Docker image
-    logging.info(f"Pulling the '{DOCKER_IMAGE}' Docker image...")
+    logging.info("Pulling the '%s' Docker image...", DOCKER_IMAGE)
     run_command(f"sudo docker pull {DOCKER_IMAGE}")
 
     # Export the container's filesystem to a tarball
     tarball_file = tmp_path / "myimage_latest.tar.gz"
-    logging.info(f"Exporting the container filesystem to {tarball_file}...")
+    logging.info("Exporting the container filesystem to '%s'...", tarball_file)
     run_command(f"sudo docker save {DOCKER_IMAGE}:latest | gzip > {tarball_file}")
 
     # Remove the container to clean up
@@ -93,7 +93,7 @@ def create_config_and_tarball(tmp_path):
     config_file = tmp_path / "config_dockertball.json"
     with open(config_file, "w") as f:
         json.dump(config_data, f, indent=4)
-    logging.info(f"Configuration file created: {config_file}")
+    logging.info("Configuration file created: '%s'", config_file)
 
     return str(config_file), str(tarball_file)
 
@@ -104,8 +104,8 @@ def test_debug_create_config_and_tarball(create_config_and_tarball):
     config_file, tarball_file = create_config_and_tarball
 
     # Log the outputs for debugging
-    logging.info(f"Config file path: {config_file}")
-    logging.info(f"Tarball file path: {tarball_file}")
+    logging.info("Config file path: '%s'", config_file)
+    logging.info("Tarball file path: '%s'", tarball_file)
 
     # Assert that the files were created successfully
     assert config_file is not None, "Config file was not created"
@@ -131,7 +131,7 @@ def test_surfactant_generate(setup_environment, create_config_and_tarball, tmp_p
         config_out = json.load(f)
     logging.info(json.dumps(config_out, indent=4))
     command = f"surfactant generate {config_file} {output_enabled_sbom}"
-    logging.info(f"Running command: {command}")
+    logging.info("Running command: '%s'", command)
     run_command(command)
 
     # Verify the SBOM file is created
@@ -165,7 +165,7 @@ def test_surfactant_generate(setup_environment, create_config_and_tarball, tmp_p
     # Run the Surfactant generate command (with Grype disabled)
     output_disabled_sbom = tmp_path / "docker_tball_grype-disabled_sbom.json"
     command = f"surfactant generate {config_file} {output_disabled_sbom}"
-    logging.info(f"Running command: {command}")
+    logging.info("Running command: '%s'", command)
     run_command(command)
 
     # Verify the SBOM file is created
