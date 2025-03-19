e92b5f5b31a405d59ab6f7b4e889b50ed82c78ab
Willis Berrios <berrios1@llnl.gov>
2025-03-19 16:14:12 +0000

    Renamed pytest fixture.

--- plugins/grype/tests/test_grype.py
+++ plugins/grype/tests/test_grype.py
@@ -19,20 +19,20 @@ def run_command(command: str) -> str:
 
 
 @pytest.fixture(scope="session")
-def setup_environment():
-    """Fixture to install Grype and the Grype plugin."""
+def setup_environment_fixture():
+    """Fixture to install Grype dependency."""
     check_command_availability("surfactant")
     check_command_availability("docker")
 
     # Verify and install required tools
     install_grype()
 
-    # Install the Grype plugin in editable mode
-    logging.info("Installing the Grype plugin in editable mode ...")
-    output = run_command("pip install -e .")
-    logging.info("Install output: '%s'", output)
+    # # Install the Grype plugin in editable mode
+    # logging.info("Installing the Grype plugin in editable mode ...")
+    # output = run_command("pip install -e .")
+    # logging.info("Install output: '%s'", output)
 
-    enable_plugin(PLUGIN_NAME)
+    # enable_plugin(PLUGIN_NAME)
 
 
 def check_command_availability(command):
@@ -70,8 +70,8 @@ def disable_plugin(plugin_name):
     assert plugin_name in output, f"{plugin_name} not found in disabled plugins"
 
 
-@pytest.fixture(scope="function")
-def create_config_and_tarball(tmp_path):
+@pytest.fixture(scope="session")
+def create_config_and_tarball_fixture(tmp_path):
     """
     Fixture to create the configuration file and Docker tarball for testing.
     The tarball contains the 'hello-world' Docker container filesystem.
@@ -99,10 +99,10 @@ def create_config_and_tarball(tmp_path):
     return str(config_file), str(tarball_file)
 
 
-def test_debug_create_config_and_tarball(config_and_tarball_fixture):
+def test_debug_create_config_and_tarball(create_config_and_tarball_fixture):
     # pytest test_grype.py -k test_debug_create_config_and_tarball -v
     # Call the fixture and unpack its return values
-    config_file, tarball_file = config_and_tarball_fixture
+    config_file, tarball_file = create_config_and_tarball_fixture
 
     # Log the outputs for debugging
     logging.info("Config file path: '%s'", config_file)
@@ -113,11 +113,11 @@ def test_debug_create_config_and_tarball(config_and_tarball_fixture):
     assert tarball_file is not None, "Tarball file was not created"
 
 
-def test_surfactant_generate(setup_env_fixture, config_and_tarball_fixture, tmp_path):
+def test_surfactant_generate(setup_environment_fixture, create_config_and_tarball_fixture, tmp_path):
     """Test the Surfactant generate command with the Grype plugin."""
     # Get the configuration file and tarball file from the fixture
 
-    config_file, tarball_file = config_and_tarball_fixture
+    config_file, tarball_file = create_config_and_tarball_fixture
 
     # **********************
     # **** Enabled Test ***
