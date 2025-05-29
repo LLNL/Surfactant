e818133940d963f26fc5c47dc51e90be614944bc
Seth Bredbenner <bredbenner1@llnl.gov>
2025-05-29 15:17:34 -0700

    Update: added exported function scanning in Plugin

--- plugins/angrimportfinder/surfactantplugin_angrimportfinder.py
+++ plugins/angrimportfinder/surfactantplugin_angrimportfinder.py
@@ -43,24 +43,27 @@ def angrimport_finder(sbom: SBOM, software: Software, filename: str, filetype: s
     if existing_json:
         with open(existing_json, "r") as json_file:
             existing_data = json.load(json_file)
-        if "imported function names" in existing_data:
+        if "imported function names" in existing_data and "exported function names" in existing_data:
             logger.info(f"Already extracted {filename.name}")
         else:
             try:
                 logger.info(
-                    f"Found existing JSON file for {filename.name} but without 'imported functions' key. Proceeding with extraction."
+                    f"Found existing JSON file for {filename.name} but without 'imported function names' or 'exported function names' keys. Proceeding with extraction."
                 )
                 # Add your extraction code here.
                 if filename.name not in existing_data["filename"]:
                     existing_data["filename"].append(filename.name)
                 existing_data["imported function names"] = []
+                existing_data["exported function names"] = []
                 # Create an angr project
                 project = angr.Project(filename, auto_load_libs=False)
 
                 # Get the imported functions using symbol information
                 for symbol in project.loader.main_object.symbols:
-                    if symbol.is_function:
-                        existing_data["imported function names"].append(symbol.name)
+                    if symbol.is_import:
+                        metadata["imported function names"].append(symbol.name)
+                    elif symbol.is_export:
+                        metadata["exported function names"].append(symbol.name)
 
                 # Write the string_dict to the output JSON file
                 with open(output_name, "w") as json_file:
@@ -79,12 +82,15 @@ def angrimport_finder(sbom: SBOM, software: Software, filename: str, filetype: s
             metadata["sha256hash"] = filehash
             metadata["filename"] = [filename.name]
             metadata["imported function names"] = []
+            metadata["exported function names"] = []
             # Create an angr project
             project = angr.Project(filename.as_posix(), auto_load_libs=False)
-            # Get the imported functions using symbol information
+            # Get the functions using symbol information
             for symbol in project.loader.main_object.symbols:
-                if symbol.is_function:
+                if symbol.is_import:
                     metadata["imported function names"].append(symbol.name)
+                elif symbol.is_export:
+                    metadata["exported function names"].append(symbol.name)
 
             # Write the string_dict to the output JSON file
             with open(output_path, "w") as json_file:
