45c0af9fcaac235482d06877b732880131b78e7d
Ryan Mast <mast9@llnl.gov>
2025-06-30 13:08:39 -0700

    fix!: remove deprecated setup.py support for old pip versions (#440)

--- setup.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2023 Lawrence Livermore National Security, LLC
-# See the top-level LICENSE file for details.
-#
-# SPDX-License-Identifier: MIT
-
-# For compatibility with old versions of tools
-from setuptools import setup
-
-setup()
