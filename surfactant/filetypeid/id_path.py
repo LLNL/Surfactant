b4d29442349db3decf9a35ddaf1a49fa543cd220
Matt Kelley <matthewkelley22@ksu.edu>
2025-08-25 13:07:21 -0500

    Handling for no matches

--- surfactant/filetypeid/id_path.py
+++ surfactant/filetypeid/id_path.py
@@ -9,13 +9,16 @@ from typing import Optional
 from loguru import logger
 
 import surfactant.plugin
+from surfactant import ContextEntry
 
 
 @surfactant.plugin.hookimpl
-def identify_file_type(filepath: str) -> Optional[str]:
+def identify_file_type(filepath: str, context: ContextEntry|None=None) -> Optional[str]:
     _substrings = {
         "var/lib/dkpg": "DPKG Database File"
     }
     for key, value in _substrings.items():
         if re.search(key, filepath):
             return value
+    else:
+        return None
