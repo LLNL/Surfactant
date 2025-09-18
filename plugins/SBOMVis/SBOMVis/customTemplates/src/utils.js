215479d590f5b55f446e0ee261e58cac01823132
Joseph H <193976820+JosephHAtWork@users.noreply.github.com>
2025-09-18 10:48:55 -0700

    feat: SBOMVis UI improvements (#490)
    Major changes:
    * Added light mode and theme switcher
    * Reduce node overlap by modifying physics settings
    * Auto re-enable physics after loading large graphs
    * Added Favicon
    * Hide isolate nodes (nodes with no relationships) toggle
    
    Minor changes:
    * Fixed logo positioning when sidebar is open
    * Reduced number of graph updates / re-rendering steps
    * Fixed issue in batch update logic where certain attributes would get clobbered
    * Changed cursor to use text when hovering over a foldable section that has no children instead of pointer

--- plugins/SBOMVis/SBOMVis/customTemplates/src/utils.js
+++ plugins/SBOMVis/SBOMVis/customTemplates/src/utils.js
@@ -4,18 +4,66 @@
  * @param {(String|undefined)} color
  */
 export function setNodeColors(nodeIDs, color) {
-	const nodesDataset = nodes.get(nodeIDs, { returnType: "Object" });
+	nodes.update(
+		nodes.get(nodeIDs).map((n) => ({
+			id: n.id,
+			color: color !== undefined ? color : n.originalColor,
+		})),
+	);
+}
 
-	for (const nID in nodesDataset) {
-		if (color !== undefined) nodesDataset[nID].color = color;
-		else {
-			nodesDataset[nID].color = nodesDataset[nID].originalColor;
-		}
-	}
+/**
+ * Sets CSS color scheme, dark/light mode toggle icon, and updates node font colors
+ * @param {String} mode (dark, light, auto)
+ */
+export function setColorScheme(mode) {
+	const toggle = document.getElementById("themeToggle");
+	const icon = toggle.querySelector("i");
 
-	const tmp = [];
-	for (const nID in nodesDataset)
-		if (Object.hasOwn(nodesDataset, nID)) tmp.push(nodesDataset[nID]);
+	const html = document.querySelector("html");
 
-	nodes.update(tmp);
+	// Update icon and CSS color-scheme
+	const colorSchemes = {
+		dark: {
+			icon: "fa-solid fa-sun",
+			tooltip: "Toggle light mode",
+			CSSColorScheme: "dark",
+		},
+		light: {
+			icon: "fa-solid fa-moon",
+			tooltip: "Toggle dark mode",
+			CSSColorScheme: "light",
+		},
+	};
+
+	let preferredColorScheme = mode;
+	if (mode === "auto") {
+		preferredColorScheme = window.matchMedia("(prefers-color-scheme: dark)")
+			.matches
+			? "dark"
+			: "light"; // Use system preferred color
+		html.style.setProperty("color-scheme", "light dark");
+	} else html.style.setProperty("color-scheme", mode);
+
+	const { icon: iconClass, tooltip } = colorSchemes[preferredColorScheme];
+	icon.classList = iconClass;
+	toggle.setAttribute("title", tooltip);
+
+	// Update node font colors
+	const styles = getComputedStyle(document.body);
+
+	const newIconColor = styles.getPropertyValue(
+		preferredColorScheme === "dark" ? "--darkNodeColor" : "--lightNodeColor",
+	);
+	const newLabelColor = styles.getPropertyValue(
+		preferredColorScheme === "dark" ? "--darkTextColor" : "--lightTextColor",
+	);
+
+	nodes.update(
+		nodes.get().map((n) => ({
+			id: n.id,
+			icon: { ...n.icon, color: newIconColor },
+			font: { ...n.font, color: newLabelColor },
+		})),
+	);
 }
