910714bf60fc4c1021e9912169dc5d7da153a60a
Joseph H <193976820+JosephHAtWork@users.noreply.github.com>
2025-10-15 12:31:56 -0700

    feat: add SBOM vis context menu to pin and delete nodes (#516)
    style: Formatting/linting missed by older version of Biome
    refactor: Register click event function
    refactor: Move mouse event handlers to seperate file
    refactor: Move node toggling to function in utils
    feat: Add custom context menu
    feat: Add back pinning behavior
    feat: Add node deletion click handler
    fix: Opening sidebar needs extra click

--- plugins/SBOMVis/SBOMVis/customTemplates/src/main.js
+++ plugins/SBOMVis/SBOMVis/customTemplates/src/main.js
@@ -1,13 +1,16 @@
+import { setButtonEventHandlers } from "#buttonEventHandlersModule";
 import {
-	setButtonEventHandlers,
-	toggleSidebar,
-} from "#buttonEventHandlersModule";
+	clickEventHandler,
+	contextMenuEventHandler,
+	doubleClickEventHandler,
+} from "#mouseEventHandlersModule";
 import { createPopupElement } from "#popupModule";
+import { buildSBOMOverviewSidebar } from "#sidebarModule";
 import {
-	buildNodeSelectionSidebar,
-	buildSBOMOverviewSidebar,
-} from "#sidebarModule";
-import { setColorScheme, setNodeColors } from "#utilsModule";
+	changeCursor,
+	registerClickHandlers,
+	setColorScheme,
+} from "#utilsModule";
 
 function drawGraph() {
 	const container = document.getElementById("mynetwork");
@@ -53,104 +56,8 @@ function drawGraph() {
 		});
 	}
 
-	function registerClickHandlers(onClick, onDoubleClick) {
-		const clickThreshold = 250;
-		let lastClickTime = 0;
-
-		function clickHandler(e) {
-			const newClickTime = new Date();
-			if (newClickTime - lastClickTime > clickThreshold) {
-				setTimeout(() => {
-					if (newClickTime - lastClickTime > clickThreshold) onClick(e);
-				}, clickThreshold);
-			}
-		}
-
-		function doubleClickHandler(e) {
-			lastClickTime = new Date();
-			onDoubleClick(e);
-		}
-
-		network.on("click", clickHandler); // Vis.js doesn't distinguish between single clicks and double clicks: https://github.com/almende/vis/issues/203
-		network.on("doubleClick", doubleClickHandler);
-	}
-
-	function onClick(params) {
-		const nodeID = params.nodes[0];
-
-		if (params.nodes.length !== 0) {
-			// Reset selection highlight if use clicks away from search results
-			const searchIsActive = !!document
-				.getElementById("sidebar")
-				.querySelector("#resultsSection");
-			if (searchIsActive) setNodeColors(nodes.getIds());
-
-			document
-				.getElementById("sidebar")
-				.replaceChildren(buildNodeSelectionSidebar(nodeID));
-		}
-
-		const sidebar = document.getElementById("sidebar");
-		if (params.nodes.length !== 0 && !sidebar.classList.contains("open"))
-			// Click on node to open sidebar
-			toggleSidebar();
-
-		if (params.nodes.length === 0 && sidebar.classList.contains("open"))
-			// Click on canvas to close sidebar
-			toggleSidebar();
-	}
-	function onDoubleClick(params) {
-		if (params.nodes.length > 0) {
-			const nodeID = params.nodes[0];
-			const clickedNode = nodes.get(nodeID);
-
-			// Expand / retract child nodes of container when double clicked
-			if (network.isCluster(nodeID)) network.openCluster(nodeID);
-			else {
-				if (clickedNode.nodeMetadata.type === "Container") {
-					const childNodes = network.getConnectedNodes(nodeID, "from");
-
-					const clusterOptions = {
-						joinCondition: (parentNodeOptions, childNodeOptions) => {
-							return childNodes.includes(childNodeOptions.id); // Only cluster if ID matches
-						},
-						clusterNodeProperties: {
-							id: `cluster:${nodeID}`,
-							label: clickedNode.nodeMetadata.nodeFileName,
-							surfactantSoftwareStruct: clickedNode.surfactantSoftwareStruct,
-							nodeMetadata: clickedNode.nodeMetadata,
-							shape: "dot",
-							icon: {
-								face: "'Font Awesome 6 Free'",
-								weight: "900",
-								code: "\uf1c6", // fa-file-zipper
-								size: clickedNode.icon.size,
-								color: clickedNode.icon.color,
-							},
-							font: {
-								color: "white",
-							},
-							title: document.getElementById("tooltip"),
-							fixed: { x: false, y: false },
-						},
-					};
-					network.clusterByConnection(nodeID, clusterOptions); // Only clusters immediate child nodes
-				}
-			}
-		}
-	}
-	registerClickHandlers(onClick, onDoubleClick);
-
-	const networkCanvas = document
-		.getElementById("mynetwork")
-		.getElementsByTagName("canvas")[0];
-
-	function changeCursor(newCursorStyle) {
-		networkCanvas.style.cursor = newCursorStyle;
-	}
-	function getCursor() {
-		return networkCanvas.style.cursor;
-	}
+	registerClickHandlers(clickEventHandler, doubleClickEventHandler);
+	network.on("oncontext", contextMenuEventHandler);
 
 	network.on("hoverNode", () => {
 		changeCursor("grab");
@@ -168,56 +75,6 @@ function drawGraph() {
 
 	network.on("showPopup", createPopupElement);
 
-	// Right click to pin a node
-	network.on("oncontext", function (params) {
-		if (getCursor() === "grabbing") return; // Don't try to pin if a node is grabbed
-
-		const nodeID = this.getNodeAt(params.pointer.DOM);
-		if (nodeID === undefined) return; // Didn't click on a node
-
-		if (network.isCluster(nodeID)) {
-			const node = network.body.nodes[nodeID];
-			const fixed = node.options.fixed;
-			const shouldBeFixed = !(fixed.x === true && fixed.y === true);
-
-			network.clustering.updateClusteredNode(nodeID, { fixed: shouldBeFixed });
-			if (shouldBeFixed)
-				network.clustering.updateClusteredNode(nodeID, {
-					label: `📌 ${node.options.nodeMetadata.nodeFileName}`,
-				});
-			else
-				network.clustering.updateClusteredNode(nodeID, {
-					label: node.options.nodeMetadata.nodeFileName,
-				});
-
-			return;
-		}
-
-		const node = nodes.get(nodeID);
-		const isFixed = node.fixed;
-
-		if (isFixed === undefined || isFixed === false) {
-			nodes.update({
-				id: nodeID,
-				fixed: true,
-				label: `📌 ${node.nodeMetadata.nodeFileName}`,
-			});
-		} else {
-			nodes.update({
-				id: nodeID,
-				fixed: false,
-				label: node.nodeMetadata.nodeFileName,
-			});
-		}
-
-		// Update popup if it's visible
-		if (
-			document.getElementsByClassName("vis-tooltip")[0].style.visibility ===
-			"visible"
-		)
-			createPopupElement(nodeID);
-	});
-
 	return network;
 }
 
@@ -230,7 +87,7 @@ document.addEventListener("DOMContentLoaded", () => {
 				setColorScheme("auto"); // Set init color scheme
 				window
 					.matchMedia("(prefers-color-scheme: dark)")
-					.addEventListener("change", (e) => {
+					.addEventListener("change", () => {
 						setColorScheme("auto"); // If the user changes the system color scheme follow that, otherwise use manually set value
 					});
 
